6b9e6bf86610c901553f8ca71e49b2533c27bd31
Shukurullo-Faxriddinovich <shukurulloasatullayev1@gmail.com>
2022-02-20 00:30:03 +0500

    Add a string to the js file

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var a = prompt("yo'l xaqini kiriting? ($)");
 var b = prompt("Mehmonxona to'lovi kiriting? ($)");
 var c = prompt("qoshimcha sarf xarajat uchun summa kiriting? (yevro)");
 var mony = prompt("Pulingizni kiriting? (so'm)");
-console.log(mony);
+console.log(mony + " so'm");
 
 var x = (a * 10.900);
 var y = (b * 10.900);
